6f72be3a9d6247a5e9ab89f57f44c15df7bd5597
fahad9988 <fahad20830@gmail.com>
2023-04-09 04:10:07 +0530

    added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 px-8">
       <p className="text-sm text-center">
-        Made with ❤️ by your name &copy; {new Date().getFullYear()}
+        Made with ❤️ by Team Keyboard Warrior &copy; {new Date().getFullYear()}
       </p>
     </footer>
   );
