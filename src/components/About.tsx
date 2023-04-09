ba9944c8c97e7878624b836bfdab4a126274ec61
fahad9988 <fahad20830@gmail.com>
2023-04-09 19:34:09 +0530

    final addition

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
+const About: React.FC = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="mb-4">
+        Welcome to Keyboard Warrior, where two players compete to see who is faster at typing! Our game was developed by four enthusiastic programmers - Owaise, Fahad, Chandra Sekhar, and Sahil - as part of a 48-hour hackathon project. We came up with the idea for this game because we wanted to create a fun and engaging way for people to improve their typing skills.
+      </p>
+      <p className="mb-4">
+        Our game is easy to play, but challenging enough to keep you coming back for more. Start typing away! Our team has put a lot of effort into making this game as enjoyable as possible for our users. We’ve designed it to be intuitive and user-friendly, with sleek graphics and smooth gameplay so everyone can have fun playing our game. We’re proud of what we’ve accomplished in such a short amount of time.
+      </p>
+      <p className="mb-4">
+        We hope that our typing game will help people improve their typing skills while also having fun. So why not give it a try and see how fast you can type? Thank you for visiting our website, and we hope you enjoy playing our game!
+      </p>
+      <div>
+        <img width={200} src='https://gifdb.com/images/file/3d-fast-keyboard-typing-b1m81hlq3dy8k03w.gif' alt="image" />
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-const Instructions = () => {
+const Instructions: React.FC = () => {
   return (
-    <div>Instructions</div>
-  )
-}
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">Instructions</h1>
+      <p className="mb-4">
+        Keyboard Warrior is a multiplayer game where each player gets a chance to type a randomly generated string within 10 seconds. If the typed string matches the generated string, the player will be awarded one point. The next player will then get a chance with a newly generated string. The first player to reach a score of 5 points wins the game.
+      </p>
+      <h2 className="text-2xl font-bold mb-2">How to Play</h2>
+      <ol className="list-decimal list-inside mb-4">
+        <li>Type your name to join the game</li>
+        <li>Wait for another player to join the game</li>
+        <li>A random string will be generated for the first player to type</li>
+        <li>Type the string before the 10 second timer runs out</li>
+        <li>If your string matches the generated string, you'll be awarded one point</li>
+        <li>The next player will get a chance with a newly generated string</li>
+        <li>The first player to reach a score of 5 points wins the game</li>
+      </ol>
+      <h2 className="text-2xl font-bold mb-2">Scoring</h2>
+      <p className="mb-4">
+        Each player starts with 0 points. If you type the generated string correctly within the 10 second timer, you'll be awarded one point. The first player to reach a score of 5 points wins the game.
+      </p>
+      <h2 className="text-2xl font-bold mb-2">Technical Requirements</h2>
+      <p className="mb-4">
+        Keyboard Warrior requires a modern web browser with JavaScript enabled. The game is optimized for desktop and laptop computers, but should work on most mobile devices as well.
+      </p>
+    </div>
+  );
+};
 
-export default Instructions
+export default Instructions;
